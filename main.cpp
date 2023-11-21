c4d7066f5798c14e2515fb137ed44f25a192351f
apichard <apichard@Gaming.tech>
2023-11-21 17:03:59 +0100

    init

--- main.cpp
+++ main.cpp
@@ -1,44 +1,39 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
 
 int main(int argc, char** argv)
 {
-    //Création d'une fenêtre
-    sf::RenderWindow oWindow(sf::VideoMode(640, 480), "SFML");
+    //création de la fenetre
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
 
-    //Création d'un cercle de radius 100
-    sf::CircleShape oCircle(100.f);
-    //A la position 0, 0
-    oCircle.setPosition(0.f, 0.f);
-    //Et de couleur verte
-    oCircle.setFillColor(sf::Color::Green);
-
-    //Création d'un rectangle de taille 50, 50
-    sf::RectangleShape oRectangle(sf::Vector2f(50.f, 50.f));
-    //A la position 100, 100
-    oCircle.setPosition(100.f, 100.f);
-    //Et de couleur rouge
-    oRectangle.setFillColor(sf::Color::Red);
+    float deltaTime = 0;
 
     //GameLoop
-    while (oWindow.isOpen())
+    while (window.isOpen())
     {
         //EVENT
         sf::Event oEvent;
-        while (oWindow.pollEvent(oEvent))
+        while (window.pollEvent(oEvent))
         {
             if (oEvent.type == sf::Event::Closed)
-                oWindow.close();
+                window.close();
+            if (oEvent.type == sf::Event::MouseButtonReleased)
+            {
+               
+            }
         }
+        // UPDATE
+      
+        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
 
-        //UPDATE
 
         //DRAW
-        oWindow.clear();
+        window.clear();
 
-        oWindow.draw(oCircle);
-        oWindow.draw(oRectangle);
+        
 
-        oWindow.display();
+        window.display();
     }
 
     return 0;
